c39330077f37bbfb6e163ad672cd5c02f9537f10
agalexei <43635477+agalexei@users.noreply.github.com>
2021-03-03 16:44:10 +1100

    updated

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ $(window).scroll(function(){
 });
 
 var initialSrc = "images/color_logo_White_logo_Transparent.png";
-var scrollSrc = "images/color_logo_Black_logo_transparent.png";
+var scrollSrc = "images/color_logo_orange_logo_Transparent.png";
 
 $(window).scroll(function() {
    var value = $(this).scrollTop();
